2034c9502eb8732bc18425c7ad53bfdfddebea79
zoharkiks <zoharkiks@gmail.com>
2022-08-15 14:59:02 +0530

    Add logo asset

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Icon } from '@iconify/react';
+import {images} from '../constants'
 
 
 const Navbar = () => {
   return (
     <div className='bg-lightGray px-4 py-4'>
 <Icon icon="bytesize:menu" className='h-8 w-8' />
-
+<img src={images.logo.src} alt="" />
     </div>
   )
 }
